6b203c33003035f432c1d2915d37d18ea0a9c04d
Julien Pecqueur <jpec@julienpecqueur.net>
2015-03-05 10:38:54 +0100

    Add dynamic args (omxplayer and multidir)

--- pimp.py
+++ pimp.py
@@ -7,9 +7,9 @@ PiMP is a simple media player designed for the Raspberry Pi.
 It works in console mode and use OMXPLAYER backend for hardware video 
 acceleration.
 
-Author:  Julien Pecqueur (JPEC)
-Email:   jpec@julienpecqueur.net
-Home:    http://raspyplayer.org
+Author:  Julien Pecqueur
+Email:   julien@peclu.net
+Home:    http://peclu.net/~pimp
 Sources: https://github.com/jpec/pimp
 
 This software is provided without any warranty. 
@@ -22,18 +22,12 @@ Have fun.
 Julien
 
 """
-
-VERSION = 0.4
+VERSION = 0.5
 
 # Allowed movies extensions
 EXTENSIONS = ["avi", "mpg", "mp4", "mkv"]
 
-# OMXPLAYER options
-# -o : output [local|hdmi]
-# -t : enable subtitles [on|off]
-# --align : subtitles aligment [center|left|right]
-OPTIONS = '-o local -t on --align center'
-
+# Keys definition
 K_NEXT="k"
 K_PREV="i"
 K_NPAG="h"
@@ -42,6 +36,7 @@ K_PLAY="p"
 K_SCAN="R"
 K_QUIT="Q"
 K_FIND="f"
+
 
 import curses
 from sys import argv
@@ -53,15 +48,28 @@ from os.path import expanduser
 from os.path import basename
 
 
-def play(movie):
-    "Play a movie"
-    cmd = 'omxplayer {0} \"{1}\" > .omx.log'
+def compute_args(args):
+    "Compute command line"
+    cmd = ""
+    for a in args:
+        cmd = cmd + a + " "
+    return(cmd)
+
+
+def get_subtitle_if_exists(movie):
+    "Get subtitle file"
     sub = movie[0:-3] + "srt"
     if isfile(sub):
-        options = OPTIONS + ' --subtitles \"{0}\"'.format(sub)
-    else:
-        options = OPTIONS
-    call(cmd.format(options, movie), shell=True)
+        return('--subtitles \"{0}\" '.format(sub))
+    return("")
+
+
+def play(movie, args):
+    "Play a movie"
+    subtitle = get_subtitle_if_exists(movie)
+    options = compute_args(args)
+    cmd = 'omxplayer {0}{1} \"{2}\" > .omx.log'
+    call(cmd.format(options, subtitle, movie), shell=True)
     return(movie)
 
 
@@ -82,13 +90,14 @@ def scan_dir_movies_for_movies(dir_movies):
 def get_movies_from_dir_movies(dir_movies):
     "Get movies from dir_movies directories and return a dictionary."
     dic_movies = dict()
-    if isdir(dir_movies):
-        lst_paths = scan_dir_movies_for_movies(dir_movies)
-        if not lst_paths:
-            return(False)
-        for p in lst_paths:
-            dic_movies[basename(p)] = p
-        return(dic_movies)
+    for cur_dir in dir_movies:
+        if isdir(car_dir):
+            lst_paths = scan_dir_movies_for_movies(cur_dir)
+            if not lst_paths:
+                return(False)
+            for p in lst_paths:
+                dic_movies[basename(p)] = p
+            return(dic_movies)
 
 
 def get_movies_from_db(db):
@@ -120,20 +129,25 @@ class PiMP(object):
         "Initialization."
         self.stdscr = stdscr
         self.status = "Hi guys! What's up? ;-)"
-        self.db = expanduser("~/.movies.db")
+        self.db = expanduser("~/.pimp")
         self.parse_args()
         self.init_curses()
         self.reload_database()
         self.get_key_do_action()
 
-
     def parse_args(self):
         "Parse command line parameters."
-        self.dir_movies = expanduser("~/movies")
+        self.dir_movies = list()
+        self.omx_args = list()
         for a in argv:
             if isdir(a):
-                self.dir_movies = a
-
+                self.dir_movies.append(a)
+            else:
+                self.omx_args.append(a)
+        if len(self.dir_movies) == 0:
+            self.dir_movies.append(expanduser("~/movies"))
+        if len(self.omx_args) == 0:
+            self.omx_args = ["-o", "both", "-t", "on", "--align", "center"]    
 
     def init_curses(self):
         "Init curses settings."
@@ -144,7 +158,6 @@ class PiMP(object):
         self.H = y
         self.W = x
 
-
     def reload_database(self, force=None):
         "Reload database (if force, rescan directories)."
         self.init_cursor()
@@ -154,14 +167,12 @@ class PiMP(object):
         self.draw_status("Library reloaded.", True)
         self.draw_window()
 
-
     def init_cursor(self):
         "Init cursor settings."
         self.cursor = dict()
         self.cursor['current'] = 0
         self.cursor['first'] = 0
         self.cursor['show'] = self.H - 2
-
 
     def load_dic_movies(self, force=False):
         "Load dic_movies."
@@ -170,14 +181,12 @@ class PiMP(object):
             self.dic_movies = get_movies_from_dir_movies(self.dir_movies)
             save_movies_to_db(self.db, self.dic_movies)
 
-
     def load_lst_movies(self):
         "Load lst_movies."
         self.lst_movies = list()
         for movie in self.dic_movies:
             self.lst_movies.append(movie)
         self.lst_movies.sort()
-
 
     def draw_window(self):
         "Draw window."
@@ -204,14 +213,12 @@ class PiMP(object):
         self.draw_status(self.status)
         self.stdscr.refresh()
 
-
     def draw_status(self, text, force=False):
         "Draw the status line"
         self.status = text
-        self.draw_line_of_text(self.H-1, "> " + text, curses.A_REVERSE)
+        self.draw_line_of_text(self.H-1, "# " + text, curses.A_REVERSE)
         if force:
             self.stdscr.refresh()
-
 
     def draw_line_of_text(self, pos, text, color):
         "Draw a line of text on the window."
@@ -223,11 +230,9 @@ class PiMP(object):
             self.stdscr.addstr(pos, 0, text)
         self.stdscr.clrtoeol()
 
-
     def get_current_movie(self):
         "Return the selected movie."
         return(self.lst_movies[self.cursor['current']])
-
 
     def scroll_to(self, offset):
         "Scroll to offset."
@@ -238,14 +243,12 @@ class PiMP(object):
         else:
             return(False)
 
-
     def scroll_up(self, nb_lines):
         "Scroll up the list of movies."
         if self.cursor['current'] > 0:
             self.cursor['current'] -= nb_lines
         if self.cursor['current'] < self.cursor['first']:
             self.cursor['first'] -= nb_lines
-
 
     def scroll_down(self, nb_lines):
         "Scroll down the list of movies."
@@ -254,7 +257,6 @@ class PiMP(object):
         if self.cursor['current'] >= self.cursor['show'] + self.cursor['first']:
             self.cursor['first'] += nb_lines
 
-
     def play_selected_movie(self):
         "Play selected movie."
         movie = None
@@ -262,11 +264,10 @@ class PiMP(object):
         if movie:
             self.stdscr.clear()
             self.stdscr.refresh()
-            res = play(movie)
+            res = play(movie, self.omx_args)
             self.draw_status("End of movie ({0}).".format(res), True)
         else:
             self.draw_status("Oops! Cannot play selected movie.", True)
-
 
     def find_and_scroll(self):
         "Search a movie and scroll to it"
@@ -286,7 +287,6 @@ class PiMP(object):
             self.draw_status("Scrolled to movies starting with '{0}'.".format(ch), True)
         else:
             self.draw_status("Oops! No movies starts with '{0}'.".format(ch), True)
-
 
     def get_key_do_action(self):
         "Event loop."
@@ -314,4 +314,4 @@ class PiMP(object):
 # MAIN PROGRAM 
 if __name__ == '__main__':
     app = curses.wrapper(PiMP)
-# END MAIN PROGRAM 
+# END MAIN PROGRAM
